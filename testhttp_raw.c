08aab2e752f2afde9da4486b5cc780bead2abcda
gerardd33 <gerardd@interia.pl>
2020-04-25 21:24:29 +0200

    Added project plan

---
+++ testhttp_raw.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+	// TODO Sparsuj i zwaliduj argumenty
+
+	// TODO Przetworz plik tekstowy z ciasteczkami
+
+	// TODO Przygotuj requesta http
+
+	// TODO Polacz sie przez TCP z danym adresem/portem
+
+	// TODO Przeslij requesta http do serwera
+
+	// TODO Wczytaj wiersz statusu z reponse'a serwera (pierwsza linia)
+
+	// TODO Jesli inny niz 200 OK, wypisz te pierwsza linie, zakoncz program
+
+	// TODO Znajdz ciasteczka, wypisz je na stdout
+
+	// TODO Znajdz dlugosc zasobu, wypisz ja na stdout
+
+	// TODO Skrypt
+}
