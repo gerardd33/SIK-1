4ca5b4ac10fa2ab96a43fd7ef4392bce59d9d0fb
gerardd33 <gerardd@interia.pl>
2020-04-26 05:05:00 +0200

    Debugged and tested argument parsers

--- argument_parser.c
+++ argument_parser.c
@@ -1,40 +1,43 @@
 #include "argument_parser.h"
 
 
+const char* HTTP_STRING = "http";
+const char* HTTPS_STRING = "https";
+const size_t HTTPS_STRING_LEN = 5;
+
 static void parse_and_validate_address_and_port(char* string_to_parse, input_data_t* input_data) {
 	char* separator_position_pointer = strchr(string_to_parse, ':');
 	if (separator_position_pointer == NULL)
-		fatal("invalid first argument");
+		fatal("invalid address/port");
 
 	*separator_position_pointer = 0;
 	++separator_position_pointer;
 
 	input_data->connection_address = string_to_parse;
 	input_data->connection_port = separator_position_pointer;
-
-	printf("adres:%s\n", input_data->connection_address);
-	printf("port:%s\n", input_data->connection_port);
 }
 
 static void parse_and_validate_http_tested_address(char* string_to_parse, input_data_t* input_data) {
-	char* separator_position_pointer = strchr(string_to_parse, '/');
-	if (separator_position_pointer == NULL)
-		fatal("invalid third argument");
+	char protocol_type[HTTPS_STRING_LEN + 1];
+	int sscanf_result = sscanf(string_to_parse, "%[^:]", protocol_type);
 
-	++separator_position_pointer; // Second '/' in "http://".
-	*separator_position_pointer = 0;
-	++separator_position_pointer;
-	input_data->host_name = separator_position_pointer;
+	// Check if sscanf read one item properly
+	if (sscanf_result != 1)
+		fatal("invalid http tested address");
 
-	printf("host:%s\n", input_data->host_name);
-	separator_position_pointer = strchr(separator_position_pointer, '/'); // Find the first '/' after host's name.
-	if (separator_position_pointer == NULL)
-		fatal("invalid third argument");
+	bool is_https = false;
+	if (strcmp(protocol_type, HTTP_STRING) != 0) {
+		if (strcmp(protocol_type, HTTPS_STRING) != 0)
+			fatal("invalid http tested address");
+		else is_https = true;
+	}
 
-	*separator_position_pointer = 0;
-	++separator_position_pointer;
-	input_data->resource_path = separator_position_pointer;
-	printf("resource:%s\n", input_data->resource_path);
+	sscanf_result = is_https ?
+			sscanf(string_to_parse, "https://%m[^/]/%ms", &(input_data->host_name), &(input_data->resource_path))
+			: sscanf(string_to_parse, "http://%m[^/]/%ms", &(input_data->host_name), &(input_data->resource_path));
+
+	if (sscanf_result != 2)
+		fatal("invalid http tested address");
 }
 
 // Puts data from parsed arguments under passed pointer to an input_data struct.
