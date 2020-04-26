d53ab388e380eaaec964e54b8b36c7f9a9d61996
gerardd33 <gerardd@interia.pl>
2020-04-26 18:24:11 +0200

    Reimplemented header parsing; Implemented checking if chunked

--- response_processor.c
+++ response_processor.c
@@ -16,32 +16,48 @@ static bool read_status_line(FILE* socket_file) {
 	return true;
 }
 
-static void find_and_write_cookies(FILE* socket_file) {
+static void convert_header_name_to_lowercase(char* line) {
+	for (size_t index = 0; line[index] != 0 && line[index] != ':'; ++index) {
+		line[index] = (char)tolower(line[index]);
+	}
+}
+
+static bool check_if_chunked(char* line, char** header_value) {
+	sscanf(line, "transfer-encoding: %m[^\r\n]", header_value);
+	return (*header_value != NULL || strstr(*header_value, "chunked") != NULL);
+}
+
+static void check_if_cookie_and_write(char* line, char** cookie) {
+	sscanf(line, "set-cookie: %m[^;\r\n ]", cookie);
+	if (*cookie != NULL)
+		printf("%s\n", *cookie);
+}
+
+static void parse_headers_and_write_cookies(FILE* socket_file, bool* chunked) {
 	char* line = NULL;
+	char* parsed_header = NULL;
 	size_t buffer_size = 0;
 	ssize_t getline_result = 0;
+	*chunked = false;
 
 	while (true) {
 		if (getline(&line, &buffer_size, socket_file) == -1)
+			syserr("getline");
+
+		if (strcmp(line, "\r\n") == 0)
 			break;
-		// TODO zrob dobrze handling bledow getline'a
 
-		for (size_t index = 0; line[index] != 0 && line[index] != ':'; ++index) {
-			line[index] = (char)tolower(line[index]);
-		}
-
-		// TODO czy tu moga byc w koncu inne delimitery? ','? ' '?
-		char* cookie = NULL;
-		sscanf(line, "set-cookie: %m[^;\r\n]", &cookie);
-		if (cookie != NULL)
-			printf("%s\n", cookie);
-		free(cookie);
+		convert_header_name_to_lowercase(line);
+		if (check_if_chunked(line, &parsed_header))
+			*chunked = true;
+		else check_if_cookie_and_write(line, &parsed_header);
 	}
 
+	free(parsed_header);
 	free(line);
 }
 
-static void find_and_write_resource_length(FILE* socket_file) {
+static void find_and_write_resource_length(FILE* socket_file, bool chunked) {
 
 }
 
@@ -49,6 +65,7 @@ void process_server_response_and_report(FILE* socket_file) {
 	if (!read_status_line(socket_file))
 		return;
 
-	find_and_write_cookies(socket_file);
-	find_and_write_resource_length(socket_file);
+	bool chunked;
+	parse_headers_and_write_cookies(socket_file, &chunked);
+	find_and_write_resource_length(socket_file, chunked);
 }
