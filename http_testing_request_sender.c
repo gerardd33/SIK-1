1f79bc96786c15b4221aa5b7cd9ddcc447520289
gerardd33 <gerardd@interia.pl>
2020-04-26 06:21:29 +0200

    Minor changes in HTTP requesting

--- http_testing_request_sender.c
+++ http_testing_request_sender.c
@@ -1,19 +1,21 @@
 #include "http_testing_request_sender.h"
 
 const char* CRLF = "\r\n";
+// Takes two string arguments: resource path and host name.
+const char* INITIAL_HEADERS_FORMAT = "GET /%s HTTP/1.1\r\n"
+																		 "Host: %s\r\n"
+																		 "User-Agent: testhttp_raw\r\n"
+																		 "Connection: close\r\n";
 
 // TODO pozmieniaj wszedzie stdout na socket_fd
 static void send_initial_headers(int socket_fd, input_data_t* input_data) {
-	fprintf(stdout, "GET /%s HTTP/1.1\r\n"
-									 "Host: %s\r\n"
-									 "User-Agent: testhttp_raw\r\n"
-									 "Connection: close\r\n",
-										input_data->resource_path,
-										input_data->host_name);
+	int fprintf_result = fprintf(stdout, INITIAL_HEADERS_FORMAT,
+																input_data->resource_path, input_data->host_name);
 
-	int fprintf_result = fprintf(stdout, "%s", CRLF);
-	if (fprintf_result != strlen(CRLF))
-		fatal("sending http request");
+	// TODO: Jak tu sprawdzic czy nie bylo bledu w fprintfie?
+	//printf("LL:%d\n", strlen(INITIAL_HEADERS_FORMAT));
+	//if (fprintf_result != strlen(INITIAL_HEADERS_FORMAT))
+	//	fatal("sending http request");
 }
 
 static void send_cookies(int socket_fd) {
