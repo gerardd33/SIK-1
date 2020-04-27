4c33202d9852d9a278f11dbce7b15113a45b4514
gerardd33 <gerardd@interia.pl>
2020-04-27 22:50:34 +0200

    Ran clang

--- testhttp_raw.c
+++ testhttp_raw.c
@@ -4,20 +4,20 @@
 #include "tcp_client.h"
 
 void deinitialise(input_data_t *input_data, int socket_fd, FILE *socket_file) {
-	free(input_data->host_name);
-	free(input_data->resource_path);
-	fclose(socket_file);
-	close(socket_fd);
+  free(input_data->host_name);
+  free(input_data->resource_path);
+  fclose(socket_file);
+  close(socket_fd);
 }
 
 int main(int argc, char *argv[]) {
-	input_data_t input_data;
-	parse_and_validate_arguments(argc, argv, &input_data);
-	int socket_fd = establish_tcp_connection(&input_data);
-	FILE *socket_file = fdopen(socket_fd, "r+");
+  input_data_t input_data;
+  parse_and_validate_arguments(argc, argv, &input_data);
+  int socket_fd = establish_tcp_connection(&input_data);
+  FILE *socket_file = fdopen(socket_fd, "r+");
 
-	send_http_testing_request(socket_file, &input_data);
-	process_server_response_and_report(socket_file);
+  send_http_testing_request(socket_file, &input_data);
+  process_server_response_and_report(socket_file);
 
-	deinitialise(&input_data, socket_fd, socket_file);
+  deinitialise(&input_data, socket_fd, socket_file);
 }
